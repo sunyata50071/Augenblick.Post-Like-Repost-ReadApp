91c635c36855b32d57731d400d8c91e7fe4d3651
sunyata50071 <162315781+sunyata50071@users.noreply.github.com>
2025-02-27 11:39:06 +0000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,76 @@
+export const postsData = [   
+    {
+        handle: `@101ThaiBoxing ♕`,
+        profilePic: `images/boxinglady.jpg`,
+        likes: 90,
+        reposts: 30,
+        postText: `Get 50% off leading muay thai brands!!! Including Raja Boxing, 
+        Twins, Fairtex, SKS and Boon!! Shop now at www.101ThaiBoxing.co.uk 🥊🥊🥊`,
+        replies: [
+                 {
+                handle: `@AIMuayThaiFighter ✅`,
+                profilePic: `images/ai.robot.png`,
+                postText: `Need some new heavy bags. My kicks keep shattering them 👊`,
+                }
+
+        ],
+        isLiked: false,
+        isReposted: false,
+        deleted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@ThePowerOf1And1 ✅`,
+        profilePic: `images/stirner.png`,
+        likes: 50,
+        reposts: 13,
+        postText: `Just got done reading Necropolitics by Achille Mbembe.
+                    Looking to delve deeper into Biopolitics.`,
+        replies: [
+                  {
+                handle: `@KeeperOfThe7Keys ✅`,
+                profilePic: `images/srila.png`,
+                postText: `Check out Pinocchio by Giorgio Agamben.`,
+            },
+                  {
+                handle: `@WhisperingWillow801 ✅`,
+                profilePic: `images/temple.png.jpg`,
+                postText: `"Waiting for the Barbarians" by J. M. Coetzee 
+                            is highly recommended.`,
+            },
+        ],
+        isLiked: false,
+        isReposted: false,
+        deleted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@ThousandMilesFrom1Step`,
+        profilePic: `images/laozi1.png`,
+        likes: 9000,
+        reposts: 1500,
+        postText: `Nothing is softer or more flexible than water, 
+                    yet nothing can resist it.`,
+        replies: [
+            {
+                handle: `@MountainWanderer 🦅`,
+                profilePic: `images/z1.png`,
+                postText: `During our dreams we do not know we are dreaming. 
+                            We may even dream of interpreting a dream. 
+                            Only on waking do we know it was a dream. 
+                            Only after the great awakening will we realize that 
+                            this is the great dream`,
+            },
+            {
+                handle: `@ThousandMilesFrom1Step`,
+                profilePic: `images/laozi1.png`,
+                postText: `Health is the greatest possession. Contentment is the greatest treasure. 
+                            Confidence is the greatest friend. Non-being is the greatest joy.`,
+            },
+        ],
+        isLiked: false,
+        isReposted: false,
+        deleted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
